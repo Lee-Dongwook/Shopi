3d777a949aae12b92111ab5debb13e344418fc45
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-02-09 22:17:26 +0900

    feat : Auth

---
+++ packages/server/src/auth/dto/auth.input.ts
@@ -0,0 +1,13 @@
+import { InputType, Field } from '@nestjs/graphql';
+import { IsNotEmpty } from 'class-validator';
+
+@InputType()
+export class AuthDto {
+  @Field()
+  @IsNotEmpty()
+  readonly email: string;
+
+  @Field()
+  @IsNotEmpty()
+  readonly password: string;
+}
